a94785cc7df52cc34798970d43b12271de01adee
radwan503 <radwancse503@gmail.com>
2024-05-03 18:34:00 +0600

    array fill with example

--- array-fill-example-random-color/src/App.jsx
+++ array-fill-example-random-color/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import Profile from './arrayFill';
 import ColorPalette from './ColorPalette';
 
 function App() {
